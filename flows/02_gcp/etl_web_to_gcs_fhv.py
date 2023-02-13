5c5a83c0b80cafe41057c2fc630fd1ee3074c566
root <root@ny-taxi.us-east4-c.c.lucid-volt-375207.internal>
2023-02-13 02:50:47 +0000

    update flows/02_gcp/etl_web_to_gcs_fhv.py

--- flows/02_gcp/etl_web_to_gcs_fhv.py
+++ flows/02_gcp/etl_web_to_gcs_fhv.py
@@ -12,7 +12,7 @@ def fetch(dataset_url: str) -> pd.DataFrame:
     # if randint(0, 1) > 0:
     #     raise Exception
 
-    df = pd.read_csv(dataset_url)
+    df = pd.read_csv(dataset_url,compression='gzip', header=0, sep=' ', quotechar='"', error_bad_lines=False)
     return df
 
 
